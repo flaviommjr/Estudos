8ed4d117c67c0923bb7298fe3386e9e4159686ee
Flávio Magalhães <flavio_m.magalhaes@yahoo.com.br>
2020-05-15 21:58:49 -0300

    add imagens e atualização de arquivos
    add imagens preview (pasta preview) dentro da pasta imagens;
    
    atualizado sistema de exibição e atualização de data e horário;
    
    atualizado cor dos  backgrounds;
    
    atualizado estilo.css para compatibilidade com exibição das informações alteradas.

--- javascript/imagem e mensagem/script.js
+++ javascript/imagem e mensagem/script.js
@@ -1,34 +1,70 @@
 function carregar(){
     img = document.querySelector('img#img')
     titulo = document.querySelector('h1#titulo')
-    data = new Date()
-    hora = data.getHours()
-    minuto = data.getMinutes()
-    segundo = data.getSeconds()    
-    horario.innerHTML = `${hora}:${minuto}:${segundo}`
+    
+    timer = setInterval(myTimer, 1000)
+    function myTimer() {
+        
+        data = new Date()
+        semana = ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado']
+        diasem = data.getDay()
+        dia = data.getDate()
+        if(dia < 10)
+        {
+            dia = '0' + dia
+        }
+    
+        mes = data.getMonth()
+        mes++
+        if(mes < 10)
+        {
+            mes = '0' + mes
+        }
 
-    if(hora>=0 && hora<12)
-    {
-        //bom dia!
-        titulo.innerHTML = `BOM DIA!!!`
-        img.src = 'imagens/manha.png'
-        document.body.style.background = '#ffd277'
-        horario.style.color = '#ffd277'
-    }
-    else if(hora>=12 && hora<=18)
-    {
-        //boa tarde!
-        titulo.innerHTML = `BOA TARDE!!!`
-        img.src = 'imagens/tarde.png'
-        document.body.style.background = '#7b7670'
-        horario.style.color = '#7b7670'
-    }
-    else
-    {
-        //boa noite!
-        titulo.innerHTML = `BOA NOITE!!!`
-        img.src = 'imagens/noite.png'
-        document.body.style.background = '#04304b'
-        horario.style.color = '#04304b'
-    }
+        ano = data.getFullYear()
+
+        hora = data.getHours()
+        if(hora < 10)
+        {
+            hora = '0' + hora
+        }
+        minuto = data.getMinutes()
+        if(minuto < 10)
+        {
+            minuto = '0' + minuto
+        }
+
+        segundo = data.getSeconds()
+        if(segundo < 10)
+        {
+            segundo = '0' + segundo
+        }
+        
+        horario.innerHTML = `${semana[diasem]} ${dia}/${mes}/${ano} - ${hora}:${minuto}:${segundo}`
+
+        if(hora>=0 && hora<12)
+        {
+            //bom dia!
+            titulo.innerHTML = `BOM DIA!!!`
+            img.src = 'imagens/manha.png'
+            document.body.style.background = 'linear-gradient(180deg, #7cbae9, #ffffff, #fffd41, #ffd277)'
+            horario.style.color = '#ffd277'
+        }
+        else if(hora>=12 && hora<=18)
+        {
+            //boa tarde!
+            titulo.innerHTML = `BOA TARDE!!!`
+            img.src = 'imagens/tarde.png'
+            document.body.style.background = 'linear-gradient(180deg, #e77d00, #fffd41, #7b7670)'
+            horario.style.color = '#7b7670'
+        }
+        else
+        {
+            //boa noite!
+            titulo.innerHTML = `BOA NOITE!!!`
+            img.src = 'imagens/noite.png'
+            document.body.style.background = 'linear-gradient(180deg, #000000, #04304b, #000000)'
+            horario.style.color = '#04304b'
+        }
+    }    
 }
